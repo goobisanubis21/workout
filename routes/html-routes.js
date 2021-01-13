f34b87294ccc9d64bac64a49b411ff6d1469d0cd
Anthony Parrino <anthony21parrino21@gmail.com>
2021-01-13 16:36:48 -0500

    created html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const { dirname } = require("path");
+const path = require("path");
+
+module.exports = function(app) {
+    app.get("/", (req, res) => {
+        res.send("index.html");
+    });
+
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+};
