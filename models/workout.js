c7f0971c08107f258be3fc1fc4e4c4966e935845
Anthony Parrino <anthony21parrino21@gmail.com>
2021-01-13 17:04:22 -0500

    debugging models file

--- models/workout.js
+++ models/workout.js
@@ -3,34 +3,43 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Add a workout type"
-  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: true
+      },
 
-  name: {
-    type: String,
-    trim: true,
-    required: "What is the name of the workout"
-  },
+      name: {
+        type: String,
+        trim: true,
+        required: true
+      },
 
-  duration: {
-    type: Number,
-    required: "How long is the workout"
-  },
+      duration: {
+        type: Number,
+        required: true
+      },
 
-  weight: {
-    type: Number
-  },
+      weight: {
+        type: Number
+      },
 
-  reps: {
-    type: Number
-  },
+      reps: {
+        type: Number
+      },
 
-  sets: {
-    type: Number
-  }
+      sets: {
+        type: Number
+      },
+      
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
 });
 
 const Workout = mongoose.model("workout", workoutSchema);
